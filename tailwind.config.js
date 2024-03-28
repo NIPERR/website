4b5c119b1c3291085bba20d78e837c770d6bc218
NIPER <82673619+NIPERR@users.noreply.github.com>
2024-03-28 22:38:07 +0100

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: [
+  "./src/**/*.{html,js,ts,jsx,tsx}",
+],
+  theme: {
+    extend: {
+      colors: {
+        pfrcatg: '#0E9479',
+      },
+    },
+  },
+  plugins: [],
+}
